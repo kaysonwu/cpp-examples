81c272cce793a5861e109310c3bf9478ddb3592e
kaysonwu <772663139@qq.com>
2020-08-22 19:28:51 +0800

    add hello-world example

---
+++ hello-world/hello.cc
@@ -0,0 +1,16 @@
+#include <iostream>
+
+#include "say.h"
+using namespace std;
+
+int main() {
+  
+  // 设置终端字符编码
+  system("chcp 65001");
+
+  sayHello();
+
+  system("pause");
+
+  return 0;
+}
---
+++ hello-world/say.cc
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "say.h"
+
+using namespace std;
+
+void sayHello() {
+  cout << "Say Hello" << endl;
+}
